e0bbe8fb7f8b2f27d5a971e0729802e972c10d86
Daniel Bergholz <bergholz.daniel@gmail.com>
2021-05-09 13:25:11 -0300

    feat: initial setup with chakra and typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import { Text } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Acompanhe o seu semestre</title>
+      </Head>
+      <Text>Acompanhe o seu semestre</Text>
+    </>
+  )
+}
